75c0fbc92a0f28770ea84440646e3d538635b501
Jairo Morales <jaanmorales@utp.edu.co>
2019-12-23 18:43:42 -0500

    cifrar

---
+++ binaria.py
@@ -0,0 +1,11 @@
+#-*- coding: utf-8 -*-
+
+binary_search
+
+
+my_list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+number_to_find = input('Ingresa un número: ')
+
+if __name__ == '__main__':
+    print(my_list)
