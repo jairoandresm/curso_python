8136982a59e7e6fda5215a3b5bf9d04d43a2a592
Jairo Morales <jaanmorales@utp.edu.co>
2020-02-28 16:37:20 -0500

    avance de TyG

--- TdG.py
+++ TdG.py
@@ -1,4 +1,28 @@
 #-*- coding: utf-8 -*-
+class Lama:
+    def __init__(self, nombre, apellido, email):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.email = email
+#Se crea una lista vacía para guardar los datos
+
+class Datos:
+    def __init__(self):
+        self.datos = []
+
+    def add(self, nombre, apellido, email):
+        dato = Lama(nombre, apellido, email)
+        self.dato.append(datos)
+
+
+def _print_contact(self, datos):
+        print('--- * --- * --- * --- * --- * --- * --- * --- * --- ')
+        print('Nombre: {}'.format(dato.nombre))
+        print('Teléfono: {}'.format(dato.apellido))
+        print('Email: {}'.format(dato.email))
+        print('--- * --- * --- * --- * --- * --- * --- * --- * --- ')
+
+
 
 def run():
     while True:
@@ -6,18 +30,26 @@ def run():
         
         Qué deseas hacer??
         
-        [a]ñadir vehículo
-        [b]uscar vehículo por placa
+        [r]egistrar Usuario
+        [b]uscar usuario
         [s]alir
         
         '''))
 
-        if command == 'a':
-            print('vehículo añadido')
+        if command == 'r':
+            nombre =  str(input('Escribe el nombre del contacto: '))
+            apellido =  str(input('Escribe el apellido del contacto: '))
+            email =  str(input('Escribe el email del contacto: '))
+            
+            dato.add(nombre, apellido, email)
+
         elif command == 'b':
-            print('ingrese la placa que desea buscar: ')
-        else:
+            print('ingrese el nombre de la persona que desea buscar: ')
+        elif command == 's':
             print('Vuelva pronto!! ')
+        else:
+            print('Comando no encontrado ')
+            
 
 
 
